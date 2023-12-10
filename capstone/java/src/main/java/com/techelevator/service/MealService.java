88c22ddf04a69e6addc88adff6f1ebfd25869df2
Gregory Van Giezen <gregvg416@gmail.com>
2023-12-10 13:39:04 -0500

    finished the addRecipeToMeal and removeRecipeFromMeal methods in MealDao, controller, and service

--- capstone/java/src/main/java/com/techelevator/service/MealService.java
+++ capstone/java/src/main/java/com/techelevator/service/MealService.java
@@ -8,4 +8,7 @@ public interface MealService {
     List<Meal> getMeals() throws InterruptedException;
     Meal getMealById(int id) throws InterruptedException;
     Meal createMeal(Meal meal);
+    int addRecipeToMeal (int mealId, int recipeId);
+    int removeRecipeFromMeal(int mealId, int recipeId);
+
 }
